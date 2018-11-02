316cd32764a47cdef7a3f80d364f31babbffa63e
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-02 12:23:55 -0400

    added individual <li>

--- index.js
+++ index.js
@@ -13,22 +13,41 @@ const STORE = [
 // // not okay!
 // STORE = {foo: 'bar'};
 
-console.log(STORE);
+//console.log(STORE);
+
+// make each individual <li> item
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li>${item.name}</li>`;
+}
+
+function generateShoppingItemsString(shoppingList){
+  console.log("generating shopping list element");
+
+  const item = shoppingList.map((item, index) => generateItemElement(item, index));
+
+  return items.join("");
+
+}
+
+
 
 //A shopping list should be rendered to the page with all current items in STORE 
 function renderShoppingList(){
-
-//   For each item in STORE, generate a string representing an <li> with:
-// the item name rendered as inner text
-// the item's index in the STORE set as a data attribute on the <li> (more on that in a moment)
-// the item's checked state (true or false) rendered as the presence or absence of a CSS class for indicating checked items (specifically, .shopping-item__checked from index.css)
-// Join together the individual item strings into one long string
-// Insert the <li>s string inside the .js-shopping-list <ul> in the DOM.
-const shoppingListItemsString = '<li>apples</li>';
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
 
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
-  console.log('`renderShoppingList` ran');
+
+
+  //   For each item in STORE, generate a string representing an <li> with:
+  // the item name rendered as inner text
+  // the item's index in the STORE set as a data attribute on the <li> (more on that in a moment)
+  // the item's checked state (true or false) rendered as the presence or absence of a CSS class for indicating checked items (specifically, .shopping-item__checked from index.css)
+  // Join together the individual item strings into one long string
+  // Insert the <li>s string inside the .js-shopping-list <ul> in the DOM.
+
 }
 
 //You should be able to add items to the list
